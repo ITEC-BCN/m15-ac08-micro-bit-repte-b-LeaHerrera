f7cfd77b6bb03747e5c915c8d281072e6b994aac
LeaHerrera <146827129+LeaHerrera@users.noreply.github.com>
2024-11-04 18:35:45 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("D D E D G A G B ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("A G A F E D D E ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("D C C D E F E D ", 120), music.PlaybackMode.UntilDone)
+})
 basic.forever(function () {
 	
 })
